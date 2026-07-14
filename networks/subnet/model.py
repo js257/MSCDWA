488169c1656c38b786e9859ad988bca889bc9a9a
js257 <90184914+js257@users.noreply.github.com>
2026-07-14 16:15:31 +0800

    Update model.py

--- networks/subnet/model.py
+++ networks/subnet/model.py
@@ -1,1 +1,1 @@
-The core code will be released after the paper is accepted.
+The core code will be released after the paper is accepted. Should reviewers require access during the review process, we would be glad to provide it upon request.
